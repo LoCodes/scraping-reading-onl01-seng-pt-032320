ab2b9c871e2a3a93d13de13b467abafe16c59c02
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-11 07:01:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,7 @@ html = open("https://flatironschool.com/")
 # Nokogiri::HTML(html)
 doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
 
 puts doc
